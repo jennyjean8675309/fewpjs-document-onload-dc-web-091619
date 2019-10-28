9a932eff276504804315139b5b1788466812fdad
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-10-28 12:41:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('The DOM has loaded...')
+    updateDOM()
+})
+
+function updateDOM() {
+    para = document.getElementById('text').innerHTML = "This is really cool!"
+}
